316df65ec4b3477490aea7abcf7a38c2897cd58b
Paul van der Meijs <paul@lowtone.nl>
2013-06-19 19:32:29 +0200

    Renamed.

--- lowtone-media-video-vimeo.php
+++ lowtone-media-video-vimeo.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Vimeo
- * Plugin URI: http://wordpress.lowtone.nl/media-vimeo
+ * Plugin URI: http://wordpress.lowtone.nl/media-video-vimeo
  * Description: Integrate YouTube in media selector.
  * Version: 1.0
  * Author: Lowtone <info@lowtone.nl>
@@ -13,10 +13,10 @@
  * @copyright Copyright (c) 2011-2012, Paul van der Meijs
  * @license http://wordpress.lowtone.nl/license/
  * @version 1.0
- * @package wordpress\plugins\lowtone\media\vimeo
+ * @package wordpress\plugins\lowtone\media\video\vimeo
  */
 
-namespace lowtone\media\vimeo {
+namespace lowtone\media\video\vimeo {
 
 	use lowtone\content\packages\Package;
 
@@ -26,10 +26,15 @@ namespace lowtone\media\vimeo {
 		return trigger_error("Lowtone Content plugin is required", E_USER_ERROR) && false;
 
 	$__i = Package::init(array(
-			Package::INIT_PACKAGES => array("lowtone"),
+			Package::INIT_PACKAGES => array("lowtone", "lowtone\\media\\video"),
 			Package::INIT_MERGED_PATH => __NAMESPACE__,
 			Package::INIT_SUCCESS => function() {
-				
+
+				add_filter("media_upload_tabs", function($tabs) {
+					$tabs["vimeo"] = __("Vimeo", "lowtone_media_video_vimeo");
+
+					return $tabs;
+				});
 
 				return true;
 			}
